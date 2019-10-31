dee2426f32f6fd0fe1def0f435cc35e7f51e76cd
VFermat <vitor.eller30@gmail.com>
2019-10-31 09:03:44 -0300

    organized feature

---
+++ backend/src/projectsAPI/index.js
@@ -0,0 +1,16 @@
+const app = require("express")();
+const parser = require("body-parser");
+
+app.use(parser.json());
+app.use(function(req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, authorization');
+  
+    next();
+});
+
+app.listen(8080);
