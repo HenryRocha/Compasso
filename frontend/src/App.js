b10d33e59419d68374bb8c6850a6a5c82616c06e
gabriel zezze <gabrielzezze@MacBook-Pro-de-gabriel.local>
2019-11-05 15:22:51 -0300

    Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import store from "./store";
 import React from "react";
 import "./App.css";
 import { history } from "./store";
+import ConnectedRouter from "react-router-redux";
 
 function App() {
     const persistor = persistStore(store);
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,16 +2,17 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+
+import { createBrowserHistory } from "history";
 const initialState = {};
 const middleWares = [thunk];
-import { persistReducer } from "redux-persist";
 
 const persistConfig = {
     key: "Compasso",
     storage
 };
 export const history = createBrowserHistory();
-const rootReducer = createRootReducer(history); // root reducer with router state
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
