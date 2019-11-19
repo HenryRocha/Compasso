a4080952cb6f8148b0d833135bd393ec6f8fed02
PortoSamuel <38635893+PortoSamuel@users.noreply.github.com>
2019-11-19 09:10:01 -0300

    Bd gk lf sp login (#10)
    * Inicio do back do login
    
    * yarn init
    
    * Backend Login
    
    * front-login
    
    * Revert "front-login"
    
    This reverts commit 5dccc6a794a6ad60c54fcac53254d005d2c5a988.
    
    * login update
    
    * login user bug api fix

--- backend/src/loginAPI/index.js
+++ backend/src/loginAPI/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/auth', require('./controllers/authController'));
-app.use('/projects', require('./controllers/projectController'));
+app.use("/auth", require("./controllers/authController"));
+app.use("/projects", require("./controllers/projectController"));
 
-app.listen(5002);
+app.listen(3000);
