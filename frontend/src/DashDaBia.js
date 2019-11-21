233e1cdaf8d813b13ad6bc98275c95106f9275bf
Iskandar <hpaivan@gmail.com>
2019-11-21 07:54:38 -0300

    Atualizando Cards

--- frontend/src/DashDaBia.js
+++ frontend/src/DashDaBia.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import AddIcon from '@material-ui/icons/Add';
 import { createMuiTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
 import Grid from '@material-ui/core/Grid';
-import Swiper from 'swiper/js/swiper.esm.bundle';
+import Typography from '@material-ui/core/Typography';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import './components/CardDashProj.css';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 class DashDaBia extends React.Component {
     constructor(props) {
@@ -12,48 +19,9 @@ class DashDaBia extends React.Component {
         this.projects = {
             hits: []
         };
-        this.state = {
-            // dummy slides data
-            slides: (function () {
-                var slides = [];
-                for (var i = 0; i < 600; i += 1) {
-                    slides.push('Slide ' + (i + 1));
-                }
-                return slides;
-            }()),
-            // virtual data
-            virtualData: {
-                slides: [],
-            },
-        }
     }
-    // componentDidMount() {
-    //     const swiper = new Swiper('.swiper-container', {
-    //         virtual: {
-    //             slides: self.state.slides,
-    //             renderExternal(data) {
-    //                 // assign virtual slides data
-    //                 self.setState({
-    //                     virtualData: data,
-    //                 });
-    //             }
-    //         },
-    //     });
-    // }
-    render() {
-        // const self = this;
-        // const swiper = new Swiper('.swiper-container', {
 
-        //     virtual: {
-        //         slides: self.state.slides,
-        //         renderExternal(data) {
-        //             // assign virtual slides data
-        //             self.setState({
-        //                 virtualData: data,
-        //             });
-        //         }
-        //     },
-        // });
+    render() {
         const titleStyle = {
             color: "black",
             backgroundColor: null,
@@ -72,49 +40,227 @@ class DashDaBia extends React.Component {
                 }
             },
         });
+        const classes = makeStyles({
+            card: {
+                height: 345,
+                width: 345,
+            },
+            media: {
+                height: 140,
+            },
+            slide: {
+                margin: 10,
+            }
+
+        });
+        const slideStyle = {
+            margin: 5
+        };
+
+
         return (
             //   Header< nao esquecer
             <div>
-                <Grid direction="row">
+                <Grid direction="column">
                     <Grid item xs container direction="row" spacing={2} style={titleStyle} >
-                        PROJETOS EM ANDAMENTO
-                    <ThemeProvider theme={theme}>
+                        <Typography gutterBottom variant="subtitle1">
+                            PROJETO EM ANDAMENTO
+                        </Typography>
+                        <ThemeProvider theme={theme}>
                             <Fab style={buttonStyle} color="secondary" size="small" aria-label="add">
                                 <AddIcon />
                             </Fab>
                         </ThemeProvider>
                     </Grid>
+                    <CarouselProvider
+                        naturalSlideWidth={80}
+                        naturalSlideHeight={80}
+                        totalSlides={3}
+                        touchEnabled="true"
+                        visibleSlides={2}>
+                        <Slider>
+                            <div>
+                            <Slide index={0}>
+                                <Card className={classes.card}>
+                                    <div className="project_card">
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                Nome do Projeto
+                                            </Typography>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                2021
+                                            </Typography>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                Data
+                                            </Typography>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                Sicredi
+                                            </Typography>
+                                        </CardContent>
+                                    </div>
+                                    <CardActions className="seeMore">
+                                        <Button size="small" color="primary">
+                                            Ver detalhes
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Slide>
+                            </div>
+                            <Slide index={1}>
+                                <Card className={classes.card}>
+                                    <div className="project_card">
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                Nome do Projeto
+                                            </Typography>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                2022
+                                            </Typography>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                Data
+                                            </Typography>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                Sicredi
+                                            </Typography>
+                                        </CardContent>
+                                    </div>
+                                    <CardActions className="seeMore">
+                                        <Button size="small" color="primary">
+                                            Ver detalhes
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Slide>
+                            <Slide index={2}>
+                                <Card className={classes.card}>
+                                    <div className="project_card">
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                Nome do Projeto
+                                            </Typography>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                2023
+                                            </Typography>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                Data
+                                            </Typography>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                Sicredi
+                                            </Typography>
+                                        </CardContent>
+                                    </div>
+                                    <CardActions className="seeMore">
+                                        <Button size="small" color="primary">
+                                            Ver detalhes
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Slide>
+                        </Slider>
+                        {/* <ButtonBack>Back</ButtonBack>
+                        <ButtonNext>Next</ButtonNext> */}
+                    </CarouselProvider>
                 </Grid>
-                {/* <div className={swiper}> */}
-                {/* <div className="swiper-wrapper"> */}
-                {/* It is important to set "left" style prop on every slide */}
-                {/* {this.state.virtualData.slides.map((slide, index) => ( */}
-                {/* <div className="swiper-slide" */}
-                {/* key={index} */}
-                {/* >{slide}</div> */}
-                {/* ))} */}
-                {/* </div> */}
-                {/* </div> */}
-                <Grid direction="row">
+                <Grid direction="column">
                     <Grid item xs container direction="row" spacing={2} style={titleStyle}>
-                        FORM PADRÃO
-                    <ThemeProvider theme={theme}>
+                        <Typography gutterBottom variant="subtitle1">
+                            FORM PADRÃO
+                        </Typography>
+                        <ThemeProvider theme={theme}>
                             <Fab style={buttonStyle} color="secondary" size="small" aria-label="add">
                                 <AddIcon />
                             </Fab>
                         </ThemeProvider>
                     </Grid>
+                    <CarouselProvider
+                        naturalSlideWidth={80}
+                        naturalSlideHeight={80}
+                        totalSlides={3}
+                        touchEnabled="true"
+                        visibleSlides={2}>
+                        <Slider>
+                        <div>
+                            <Slide index={0}>
+                                <Card className={classes.card}>
+                                    <div className="project_card">
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                Nome do Projeto
+                                            </Typography>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                2021
+                                            </Typography>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                Data
+                                            </Typography>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                Sicredi
+                                            </Typography>
+                                        </CardContent>
+                                    </div>
+                                    <CardActions className="seeMore">
+                                        <Button size="small" color="primary">
+                                            Ver detalhes
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Slide>
+                            </div>
+                            <Slide index={1}>
+                                <Card className={classes.card}>
+                                    <div className="project_card">
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                Nome do Projeto
+                                            </Typography>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                2022
+                                            </Typography>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                Data
+                                            </Typography>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                Sicredi
+                                            </Typography>
+                                        </CardContent>
+                                    </div>
+                                    <CardActions className="seeMore">
+                                        <Button size="small" color="primary">
+                                            Ver detalhes
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Slide>
+                            <Slide index={2}>
+                                <Card className={classes.card}>
+                                    <div className="project_card">
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                Nome do Projeto
+                                            </Typography>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                2023
+                                            </Typography>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                Data
+                                            </Typography>
+                                            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+                                                Sicredi
+                                            </Typography>
+                                        </CardContent>
+                                    </div>
+                                    <CardActions className="seeMore">
+                                        <Button size="small" color="primary">
+                                            Ver detalhes
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Slide>
+                        </Slider>
+                        {/* <ButtonBack>Back</ButtonBack>
+                        <ButtonNext>Next</ButtonNext> */}
+                    </CarouselProvider>
                 </Grid>
-                {/* <div className={swiper}> */}
-                {/* <div className="swiper-wrapper"> */}
-                {/* It is important to set "left" style prop on every slide */}
-                {/* {this.state.virtualData.slides.map((slide, index) => ( */}
-                {/* <div className="swiper-slide" */}
-                {/* key={index} */}
-                {/* >{slide}</div> */}
-                {/* ))} */}
-                {/* </div> */}
-                {/* </div> */}
             </div>
         )
     }
--- frontend/src/components/CardDashProj.js
+++ frontend/src/components/CardDashProj.js
@@ -1,49 +1,57 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import './CardDashProj.css';
-const useStyles = makeStyles({
-  card: {
-    height: 345,
-    width: 345
-  },
-  media: {
-    height: 140,
-  },
-});
 
-export default function MediaCard() {
-  const classes = useStyles();
+class CardDashProj extends React.Component {
 
-  return (
-    <Card className={classes.card}>
-    <div className="project_card">
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2" className="project_title">
-            Nome do Projeto
-          </Typography>
-          <Typography gutterBottom variant="h5" component="h2" className="project_title">
-            2020
-          </Typography>
-          <Typography gutterBottom variant="h5" component="h2" className="project_title">
-            Data
-          </Typography>
-          <Typography gutterBottom variant="h5" component="h2" className="project_title">
-            Sicredi
-          </Typography>
-        </CardContent>
-    </div>
-      <CardActions className="seeMore">
-        <Button size="small" color="primary">
-          Ver detalhes
-        </Button>
-      </CardActions>
-    </Card>
-  );
+  // constructor(props) {
+  //   super(props);
+  //   this.projects = {
+  //     hits: []
+  //   };
+  // }
+  // const classes = useStyles();
+  render(){
+    const {nomeProjeto,ano,data,empresa} = this.props;
+    const classes = makeStyles({
+      card: {
+        height: 345,
+        width: 345
+      },
+      media: {
+        height: 140,
+      },
+    });
+    return(
+      <Card className = { classes.card } >
+        <div className="project_card">
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+              {nomeProjeto}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+              {ano}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+              {data}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="h2" className="project_title">
+              {empresa}
+            </Typography>
+          </CardContent>
+        </div>
+        <CardActions className="seeMore">
+          <Button size="small" color="primary">
+            Ver detalhes
+          </Button>
+        </CardActions>
+      </Card>
+    );
+  }
 }
+export default (CardDashProj);
