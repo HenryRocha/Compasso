a7c67b0f9385a0e3407b9e849dfb8928779a0574
Henry Furquim Rocha <henryfr1@al.insper.edu.br>
2019-11-25 08:26:52 -0300

    Hr ideas (#21)
    * Fix typo
    
    * Fix variable name for the /ideas route
    
    * Add nodemon module and start script (loginAPI)
    
    * Create script to run all APIs from /backend
    
    * Refactor API and add routes info on README
    
    * FIX - change router

---
+++ backend/src/ideasAPI/schemas.js
@@ -0,0 +1,61 @@
+// MODULES
+const mongoose = require("mongoose");
+
+const USER = new mongoose.Schema({
+  _projectId: mongoose.ObjectId,
+  name: String,
+  email: String,
+  salt: String,
+  hash: String,
+  admin: Boolean,
+  manager: Boolean,
+});
+
+const QUESTION = new mongoose.Schema({
+  question: String,
+  choices: Array,
+  answers: Array,
+  type: String,
+  multiple: Boolean,
+});
+
+const QUIZ = new mongoose.Schema({
+  _userId: mongoose.ObjectId,
+  _projectId: mongoose.ObjectId,
+  _templateId: mongoose.ObjectId,
+  answerDate: {type: Date, default: Date.now},
+  questions: [{type: mongoose.Schema.Types.Mixed, ref: QUESTION}],
+});
+
+const PROJECT = new mongoose.Schema({
+  title: String,
+  description: String,
+  email: String,
+  token: String,
+  quizzes: mongoose.Schema.Types.Mixed,
+});
+
+const TEMPLATE = new mongoose.Schema({
+  title: String,
+  description: String,
+  questions: Array,
+});
+
+const IDEAQUIZ = new mongoose.Schema({
+  _templateId: mongoose.ObjectId,
+  _quizId: {type: mongoose.ObjectId, default: null},
+  answered: {type: Boolean, default: false},
+  deadline: Date,
+  name: String,
+});
+
+const IDEA = new mongoose.Schema({
+  _userId: mongoose.ObjectId,
+  _projectId: mongoose.ObjectId,
+  quizzes: [{type: mongoose.Schema.Types.Mixed, ref: IDEAQUIZ}],
+});
+
+// EXPORTS
+module.exports = {
+  USER, QUIZ, PROJECT, TEMPLATE, IDEA, IDEAQUIZ,
+};
