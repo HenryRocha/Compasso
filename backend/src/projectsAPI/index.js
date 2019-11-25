5b00596c2004740a89bae76a6bdd529e8587c1c5
VFermat <vitor.eller30@gmail.com>
2019-11-24 22:55:57 -0300

    FIX - enforce coding style

--- backend/src/projectsAPI/index.js
+++ backend/src/projectsAPI/index.js
@@ -1,55 +1,58 @@
-const bodyParser = require("body-parser");
+// This API is responsible for handling all projects-related requests.
+
+
+// MODULES
+// Importing all the modules required.
+require("dotenv").config();
+const parser = require("body-parser");
 const express = require("express");
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const db = require("./db");
+const ObjectId = require("mongoose").Types.ObjectId;
 
+
+// CONSTANTS
+// Declaring the app constant and the port this API will run on.
 const app = express();
-
-var port = process.env.PORT || 3001;
-
+const port = process.env.PORT || 3001;
 
 
+// APP
+// Configure which JSON parser is to be used by the app.
+app.use(parser.json());
 
+// Configure where we can receive requests from and which methods and headers we allow.
 app.use(function(req, res, next) {
-    // Website you wish to allow to connect.
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
-  
-    // Request methods you wish to allow.
-    res.setHeader("Access-Control-Allow-Methods", "POST");
-  
-    // Request headers you wish to allow.
-    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
-  
-    next();
-  });
+  // Website you wish to allow to connect.
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 
+  // Request methods you wish to allow.
+  res.setHeader("Access-Control-Allow-Methods", "POST");
 
-app.get('/projects', function(req, res, next) { //TODO: check get request
-    var name = req.body.project;
-    var contactPoint = req.body.contact;
-    var interactions = req.body.quiz;
+  // Request headers you wish to allow.
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
 
-
+  next();
 });
 
 
+app.get("/projects", function(req, res, next) { // TODO: check get request
+  const name = req.body.project;
+  const contactPoint = req.body.contact;
+  const interactions = req.body.quiz;
+});
 
 
-app.post('/projects', function (req, res, next) {
-    const projectInfo = {
-        _companyId: new ObjectId(req.body.companyId),
-        projectsName: req.body.name,
-        contact: req.body.contact,
-    };
+app.post("/projects", function(req, res, next) {
+  const projectInfo = {
+    _companyId: new ObjectId(req.body.companyId),
+    projectsName: req.body.name,
+    contact: req.body.contact,
+  };
 
-    addProject(projectInfo).then((resp) => res.send(resp)).catch((err) => console.log(err))
-
-    
-
-
-})
+  db.addProject(projectInfo).then((resp) => res.send(resp)).catch((err) => console.log(err));
+});
 
 
 app.listen(port, function() {
-	console.log("App running");
+  console.log("App running");
 });
