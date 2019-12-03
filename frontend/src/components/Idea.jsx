52de5c2d416570c91ab8fcd9639eefe81549e7a7
gabriel zezze <gabrielzezze@me.com>
2019-12-03 02:04:24 -0300

    Too late for message

--- frontend/src/components/Idea.jsx
+++ frontend/src/components/Idea.jsx
@@ -5,26 +5,36 @@ export class Idea extends Component {
     render() {
         const idea = this.props.idea;
         return (
-            <div style={{ display: "flex", flexFlow: "column nowrap" }}>
+            <div
+                style={{
+                    display: "flex",
+                    flexFlow: "column nowrap",
+                    marginTop: "1rem"
+                }}
+            >
                 <h1> Ideia {idea.title}</h1>
                 <h1 style={{ fontWeight: "light" }}>
                     {" "}
                     Descricao: {idea.description}
                 </h1>
-                {idea.quizzes.map(quizId => {
-                    let quiz = this.props.quizzes.find(
-                        element => element._id === quizId
-                    );
-                    if (quiz) {
-                        return <h1>AQUI</h1>;
-                    } else return <h1>RUIM</h1>;
-                })}
+
+                {this.props.quizzes &&
+                    idea.quizzes.map(quizId => {
+                        let quiz = this.props.quizzes.find(
+                            quiz => quiz._id === quizId
+                        );
+                        if (quiz) {
+                            return <h1>{"Quiz: " + quiz.name}</h1>;
+                        } else return null;
+                    })}
             </div>
         );
     }
 }
 function mapStateToProps(state) {
-    return {};
+    return {
+        quizzes: state.data.quizzes
+    };
 }
 
 export default connect(mapStateToProps, null)(Idea);
