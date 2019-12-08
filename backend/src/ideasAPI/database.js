56884beb605b5a93499aff01b559c0aac43c29e9
Ubuntu <ubuntu@ip-172-31-95-106.ec2.internal>
2019-12-08 19:11:51 +0000

    CORRECT ERROR

--- backend/src/ideasAPI/database.js
+++ backend/src/ideasAPI/database.js
@@ -60,7 +60,7 @@ async function postIdea(idea) {
       idea.quizzes = quizIdList;
 
       createdIdea = await dbIdeas.create(idea);
-      idea.id = createdIdea._id;
+      idea._id = createdIdea._id;
 
       return {
         ok: true,
