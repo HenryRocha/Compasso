35bf8b5f224c46624fbcfa71cbcbc7beb4effe10
Ubuntu <ubuntu@ip-172-31-95-106.ec2.internal>
2019-12-10 12:23:54 +0000

    FIX

--- backend/src/loginAPI/index.js
+++ backend/src/loginAPI/index.js
@@ -6,6 +6,18 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+// Configure where we can receive requests from and which methods and headers we allow.
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect.
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // Request methods you wish to allow.
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET");
+  // Request headers you wish to allow.
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
+
+  next();
+});
+
 app.use("/auth", require("./controllers/authController"));
 
 app.listen(5002);
